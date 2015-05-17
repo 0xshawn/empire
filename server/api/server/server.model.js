068517d532a1caf138fd9659912aa651e977600a
Shown Tien <hightian@gmail.com>
2015-05-18 02:39:23 +0800

    modify server schema

--- server/api/server/server.model.js
+++ server/api/server/server.model.js
@@ -3,9 +3,17 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+var NICSchema = new Schema({
+  name: String,
+  privateIp: String,
+  publicIp: String
+});
+
 var ServerSchema = new Schema({
   hostname: String,
-  desciption: String
+  desciption: String,
+  nic: [NICSchema],
+  disk: [DiskSchema]
 });
 
 module.exports = mongoose.model('Server', ServerSchema);
